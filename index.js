bc684c7e0e8672bb7e49616873a300f1bc5cebf7
Guilherme Mussi <gmussi@gmail.com>
2021-08-30 12:47:11 +0200

    Add gaussian function

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 function Common() {
     this.models = {};
 
-    this.init = (mongoose) => {
+    this.initModels = (mongoose) => {
         require('./models/Bet');
         require('./models/ChatMessage');
         require('./models/Event');
         require('./models/User');
+        require('./models/CasinoTrade');
     
         this.models.User = mongoose.model("User");
         this.models.ChatMessage = mongoose.model('ChatMessage');
         this.models.Event = mongoose.model('Event');
         this.models.Bet = mongoose.model('Bet');
+        this.models.CasinoTrade = mongoose.model('CasinoTrade');
     }
+
+    this.utils = {
+        getGaussian: (mean, stdev) => {
+            return require("./utils/gaussian")(mean, stdev);
+        }
+    };
 }
 
 var common = new Common();
