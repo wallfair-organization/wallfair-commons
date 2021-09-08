55e2dc8f52cd5518cd20c034723de1f23d8463c3
Amir Zametica - External <amir.zametica@klika.ba>
2021-09-08 17:40:49 +0200

    Rework model

--- models/Trade.js
+++ models/Trade.js
@@ -3,17 +3,42 @@ const mongoose = require("mongoose");
 const tradeSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
+    ref: 'User',
+    required: true,
   },
   betId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Bet'
+    ref: 'Bet',
+    required: true,
+  },
+  outcomeIndex: {
+    type: Number,
+    required: true,
+  },
+  investmentAmount: {
+    type: Number,
+    required: true,
+  },
+  totalWin: {
+    type: Number,
+    required: true,
   },
   status: {
     type: String,
-    enum: ['open', 'closed'],
-    default: 'open'
-  }
+    enum: ['active', 'closed'],
+    default: 'active',
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model("Trade", tradeSchema);
