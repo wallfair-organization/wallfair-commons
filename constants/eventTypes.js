c32c60be17e53c569b24544d69b9b165690d4d66
Vladislav Uspenskii <alhkras@gmail.com>
2021-09-17 13:29:14 +0700

    Add universal event mode Add sample event type constants

---
+++ constants/eventTypes.js
@@ -0,0 +1,13 @@
+const notificationTypes = {
+    EVENT_START: 'Notification/EVENT_START',
+    EVENT_RESOLVE: 'Notification/EVENT_RESOLVE',
+    EVENT_CANCEL: 'Notification/EVENT_CANCEL',
+    EVENT_NEW_BET: 'Notification/EVENT_NEW_BET',
+    EVENT_ONLINE: 'Notification/EVENT_ONLINE',
+    EVENT_OFFLINE: 'Notification/EVENT_OFFLINE',
+    EVENT_NEW_REWARD: 'Notification/EVENT_NEW_REWARD',
+    EVENT_BET_PLACED: 'Notification/EVENT_BET_PLACED',
+    EVENT_BET_CASHED_OUT: 'Notification/EVENT_BET_CASHED_OUT',
+};
+
+exports.notificationTypes = notificationTypes
