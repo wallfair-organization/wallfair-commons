b63ed8465a8b29994c4d2e27de3b262981a1d2d9
Guilherme Mussi <gmussi@gmail.com>
2021-09-20 20:32:17 +0200

    Refactor UniversalEvent model

--- models/UniversalEvent.js
+++ models/UniversalEvent.js
@@ -2,25 +2,39 @@ const mongoose = require("mongoose");
 const {universalEventTypes} = require("../constants/eventTypes");
 
 const universalEventSchema = mongoose.Schema({
-    //Aggregate, if there is an entity associated with an event (i.e Bet, Game)
-    agg: {type: mongoose.Schema.Types.ObjectId},
-    //If there is a user associated with an event
-    user: {ref: 'User', type: mongoose.Schema.Types.ObjectId},
-    //A list of registered types
+    // A list of registered types
     type: {
         type: String,
+        required: true,
         enum: universalEventTypes
     },
-    //A short human readable message
-    message: {type: String},
-    //Redis channel, socket.io room, amqp routing or similar
+
+    // Originator  of this event
+    performedBy: {
+        type: String,
+        required: true,
+        enum: ['user', 'system', 'app']
+    },
+
+    // In case of User, this is ObjectID
+    // In case of System, this is microservice name
+    // In case of app, Oauth Client Id
+    userId: {
+        type: String,
+        required: true
+    },
+    
+    // Redis channel, socket.io room, amqp routing or similar
     channel: {type: String},
-    //Event payload
+
+    // Event payload
     data: {},
-    //Event source, pid, service, channel, etc.
-    metadata: {},
-    //Explicitly define event schema version
-    version: {type: Number, default: 1}
+
+    // Explicitly define event schema version
+    version: {
+        type: Number, 
+        default: 1
+    }
 },    {timestamps: true})
 
 module.exports = mongoose.model("UniversalEvent", universalEventSchema)
