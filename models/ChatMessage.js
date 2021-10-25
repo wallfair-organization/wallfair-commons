951a661687e0030465144117875d0c83d8e0aee5
Sergio Sorrentino <sosergio@gmail.com>
2021-10-25 15:12:14 +0100

    ML-139 chat message can be marked as read

--- models/ChatMessage.js
+++ models/ChatMessage.js
@@ -1,27 +1,31 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const chatMessageSchema = new mongoose.Schema({
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    roomId: {
-        type: mongoose.Schema.Types.ObjectId,
-    },
-    type: {
-        type: String,
-        required: true,
-        enum: ['GAME', 'EVENT', 'USER', 'BET_CREATE', 'BET_PLACE', 'BET_PULLOUT'],
-    },
-    message: {
-        type: String,
-        required: true,
-        max: 1200,
-    },
-    date: {
-        type: Date,
-        required: true,
-        default: Date.now,
-    }
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  roomId: {
+    type: mongoose.Schema.Types.ObjectId,
+  },
+  type: {
+    type: String,
+    required: true,
+    enum: ['GAME', 'EVENT', 'USER', 'BET_CREATE', 'BET_PLACE', 'BET_PULLOUT'],
+  },
+  message: {
+    type: String,
+    required: true,
+    max: 1200,
+  },
+  date: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  read: {
+    type: Date,
+    required: false,
+  },
 });
-module.exports = mongoose.model("ChatMessage", chatMessageSchema);
+module.exports = mongoose.model('ChatMessage', chatMessageSchema);
