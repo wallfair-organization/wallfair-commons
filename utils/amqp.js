5e29bc1e031b1d26e5c716dcd1b120860666798e
Amir Zametica - External <amir.zametica@klika.ba>
2021-12-20 16:11:39 +0100

    Add amqp utils

---
+++ utils/amqp.js
@@ -0,0 +1,28 @@
+const amqplib = require('amqplib');
+
+const rabbitUrl = process.env.RABBITMQ_CONNECTION;
+
+let connection, channel;
+
+const initAmqp = async () => {
+  connection = await amqplib.connect(rabbitUrl, {
+    heartbeat: 60,
+    noDelay: true,
+  });
+  channel = await connection.createChannel();
+};
+
+const sendMessage = async (exchange, routingKey, data) => {
+  try {
+    await channel.assertExchange(exchange, 'topic', { durable: true });
+    channel.publish(exchange, routingKey, Buffer.from(data));
+    console.log('PUBLISH %s - %s', exchange, routingKey);
+  } catch (e) {
+    console.error('Error in publishing message', e);
+  }
+};
+
+module.exports = {
+  initAmqp,
+  sendMessage,
+};
